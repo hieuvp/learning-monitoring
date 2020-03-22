b579240f9a06fadda0f5380aefa609f6682b73ec
Harrison Van <harrison.van@shopback.com>
2020-03-22 18:59:07 +0700

    refactoring

--- terraform-prometheus/main.tf
+++ terraform-prometheus/main.tf
@@ -1,8 +1,24 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+locals {
+  application = "Prometheus"
+  environment = "Test"
+
+  instance_type = "t2.micro"
+  ami           = "ami-0cbc6aae997c6538a"
+}
+
 # Create an EC2 resource on AWS
 resource "aws_instance" "this" {
-  ami           = local.ami
   instance_type = local.instance_type
+  ami           = local.ami
   key_name      = var.key_name
 
-  tags = local.tags
+  tags = {
+    Name        = "${upper(local.environment)}-${lower(local.application)}"
+    Application = local.application
+    Environment = local.environment
+  }
 }
