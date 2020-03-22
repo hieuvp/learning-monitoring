256432722c8fc11f760fcdc16ac8fe20a8f1e288
Harrison Van <harrison.van@shopback.com>
2020-03-22 21:40:46 +0700

    refactoring

--- terraform-prometheus/main.tf
+++ terraform-prometheus/main.tf
@@ -29,10 +29,9 @@ resource "aws_instance" "this" {
 resource "cloudflare_record" "this" {
   zone_id = var.cloudflare_zone_id
 
-  type  = "A"
-  value = aws_instance.this.private_ip
-
+  type    = "A"
   name    = lower(local.application)
+  value   = aws_instance.this.private_ip
   ttl     = "1"
   proxied = "false"
 }
