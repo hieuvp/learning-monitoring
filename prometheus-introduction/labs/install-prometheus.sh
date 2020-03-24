9bb562bf2bb8a1ab763f5d35acb78c83eae3badf
Harrison Van <harrison.van@shopback.com>
2020-03-24 11:47:31 +0700

    sudo

--- prometheus-introduction/labs/install-prometheus.sh
+++ prometheus-introduction/labs/install-prometheus.sh
@@ -20,27 +20,27 @@ cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
 sudo useradd --no-create-home --shell /bin/false prometheus
 
 # Create directories
-mkdir -p /etc/prometheus
-mkdir -p /var/lib/prometheus
+sudo mkdir -p /etc/prometheus
+sudo mkdir -p /var/lib/prometheus
 
 # Set ownership
-chown prometheus:prometheus /etc/prometheus
-chown prometheus:prometheus /var/lib/prometheus
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
 
 # Copy binaries
 cp prometheus /usr/local/bin/
 cp promtool /usr/local/bin/
 
-chown prometheus:prometheus /usr/local/bin/prometheus
-chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
 
 # Copy config
 cp -r consoles /etc/prometheus
 cp -r console_libraries /etc/prometheus
 cp prometheus.yml /etc/prometheus/prometheus.yml
 
-chown -R prometheus:prometheus /etc/prometheus/consoles
-chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 
 # Setup systemd
 echo '[Unit]
