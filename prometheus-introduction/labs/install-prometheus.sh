2c14cf89bed6f12534656487fbe26d17d2c39fd6
Harrison Van <harrison.van@shopback.com>
2020-03-24 11:53:27 +0700

    debugging

--- prometheus-introduction/labs/install-prometheus.sh
+++ prometheus-introduction/labs/install-prometheus.sh
@@ -6,6 +6,8 @@ set -eou pipefail
 readonly WORKING_DIRECTORY="/tmp/learning-monitoring"
 readonly PROMETHEUS_VERSION="2.16.0"
 
+set -x
+
 rm -rf "$WORKING_DIRECTORY"
 mkdir "$WORKING_DIRECTORY"
 cd "$WORKING_DIRECTORY"
@@ -17,30 +19,30 @@ cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
 #./prometheus --config.file=prometheus.yml
 
 # Create user
-# sudo useradd --no-create-home --shell /bin/false prometheus
+# useradd --no-create-home --shell /bin/false prometheus
 
 # Create directories
-sudo mkdir -p /etc/prometheus
-sudo mkdir -p /var/lib/prometheus
+mkdir -p /etc/prometheus
+mkdir -p /var/lib/prometheus
 
 # Set ownership
-sudo chown prometheus:prometheus /etc/prometheus
-sudo chown prometheus:prometheus /var/lib/prometheus
+chown prometheus:prometheus /etc/prometheus
+chown prometheus:prometheus /var/lib/prometheus
 
 # Copy binaries
 cp prometheus /usr/local/bin/
 cp promtool /usr/local/bin/
 
-sudo chown prometheus:prometheus /usr/local/bin/prometheus
-sudo chown prometheus:prometheus /usr/local/bin/promtool
+chown prometheus:prometheus /usr/local/bin/prometheus
+chown prometheus:prometheus /usr/local/bin/promtool
 
 # Copy config
 cp -r consoles /etc/prometheus
 cp -r console_libraries /etc/prometheus
 cp prometheus.yml /etc/prometheus/prometheus.yml
 
-sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+chown -R prometheus:prometheus /etc/prometheus/consoles
+chown -R prometheus:prometheus /etc/prometheus/console_libraries
 
 # Setup systemd
 echo '[Unit]
