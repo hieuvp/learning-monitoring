49f2a1630548749e8f542c4d929f6554fadb8d3b
Harrison Van <harrison.van@shopback.com>
2020-03-28 10:10:00 +0700

    extracting

--- prometheus-terraform/main.tf
+++ prometheus-terraform/main.tf
@@ -2,20 +2,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-locals {
-  application = "Prometheus"
-  environment = "Test"
-
-  instance_type = "t2.micro"
-  volume_size   = "20" # In gibibytes (GiB)
-
-  ami  = "ami-0cbc6aae997c6538a" # Amazon Linux 2
-  user = "ec2-user"
-
-  domain_id   = data.cloudflare_zones.this.zones[0].id
-  domain_name = lower(local.application)
-}
-
 # Create an EC2 instance on AWS
 resource "aws_instance" "this" {
   instance_type = local.instance_type
